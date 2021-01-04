738bfc4a3dad4d3cc9c2c6fac0f36f1c8c33ab29
Joao Victhor <joaovicthor@crescevendas.com>
2021-01-04 16:35:16 -0300

    Fixing timezone heroku 3

--- db/migrate/20201221191718_change_activity_start_time_type_to_datetime.rb
+++ db/migrate/20201221191718_change_activity_start_time_type_to_datetime.rb
@@ -1,5 +1,5 @@
 class ChangeActivityStartTimeTypeToDatetime < ActiveRecord::Migration[6.0]
   def change
-    change_column :activities, :start_time, 'datetime using start_time::timestamp'
+    change_column :activities, :start_time, "datetime using ('2021-1-1'::date + start_time::timestamptz)"
   end
 end
