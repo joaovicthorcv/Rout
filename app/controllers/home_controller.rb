93ac9732d0736498bb02d8b6a9a7dbe9083a31d2
joaovicthorcv <joaovicthor@crescevendas.com>
2023-09-22 09:20:56 -0300

    ff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @activities = Activity.all
-    @masteries = Hash[ActiveRecord::Base.connection.select_rows(Activity.where(user_id: current_user.id).select("start_time as x", "mastery as y"))]
-    @pleasures = Hash[ActiveRecord::Base.connection.select_rows(Activity.where(user_id: current_user.id).select("start_time as x", "pleasure as y"))]
-    @emotion_scales = Hash[ActiveRecord::Base.connection.select_rows(Emotion.where(user_id: current_user.id).select("created_at as x", "scale*2 as y"))]
-    @emotions = Emotion.where("created_at >= ?", Date.today)
-    @chart_period = 7
+    # @activities = Activity.all
+    # @masteries = Hash[ActiveRecord::Base.connection.select_rows(Activity.where(user_id: current_user.id).select("start_time as x", "mastery as y"))]
+    # @pleasures = Hash[ActiveRecord::Base.connection.select_rows(Activity.where(user_id: current_user.id).select("start_time as x", "pleasure as y"))]
+    # @emotion_scales = Hash[ActiveRecord::Base.connection.select_rows(Emotion.where(user_id: current_user.id).select("created_at as x", "scale*2 as y"))]
+    # @emotions = Emotion.where("created_at >= ?", Date.today)
+    # @chart_period = 7
   end
 
   def chart_period
